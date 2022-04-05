6a3cf6bce6c4822ad99214b78fa73af9cdfd667e
tobias gimenez~ <tobiasgimenezchirino@gmail.com~>
2022-04-05 11:45:15 -0300

    agregue index y algoritmo

---
+++ algoritmo.js
@@ -0,0 +1,7 @@
+console.log("hello world!");
+
+document.write("algo");
+
+document.write("<h1> LA PRIMERA </h1>")
+
+document.write("<img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcThBt0H_AYE-g42XdvkkzDO9HSdIsevVJusuA&usqp=CAU'alt='SELECCION ARGENTINA CS'>")
